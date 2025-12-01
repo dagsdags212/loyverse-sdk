addcf4a518c02b450a5fdc26730dcadcf8b012c4
dagsdags212 <jegsamson.dev@gmail.com>
2025-12-01 08:03:05 +0800

    updated datetime attributes to include timezone, as specified by config

--- src/loyverse_api/models/base.py
+++ src/loyverse_api/models/base.py
@@ -1,6 +1,8 @@
 from uuid import uuid4, UUID
 from datetime import datetime
-from pydantic import BaseModel, Field
+from zoneinfo import ZoneInfo
+from pydantic import BaseModel, Field, field_validator
+from loyverse_api.core.config import config
 
 
 class Base(BaseModel):
@@ -8,3 +10,8 @@ class Base(BaseModel):
     created_at: datetime
     updated_at: datetime
     deleted_at: datetime | None = Field(default=None, exclude=True)
+
+    @field_validator("created_at", "updated_at", "deleted_at", mode="after")
+    def update_timezone(cls, value: datetime | None) -> datetime:
+        if value:
+            return value.replace(tzinfo=ZoneInfo(config.timezone))
