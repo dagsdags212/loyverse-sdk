9992970e492c0009af05635cd32de2841a942b3e
dagsdags212 <jegsamson.dev@gmail.com>
2025-12-15 01:37:28 +0800

    fixed bug: convert UTC time to local time (Asia/Manila) or other timezone specified in config

--- src/loyverse_sdk/models/common.py
+++ src/loyverse_sdk/models/common.py
@@ -2,6 +2,7 @@ from uuid import uuid4, UUID
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from pydantic import BaseModel, Field, field_validator
+import pytz
 from loyverse_sdk.core.config import config
 
 
@@ -12,9 +13,12 @@ class Base(BaseModel):
     deleted_at: datetime | None = Field(default=None, exclude=True)
 
     @field_validator("created_at", "updated_at", "deleted_at", mode="after")
-    def update_timezone(cls, value: datetime | None) -> datetime:
+    def utc_to_local(cls, value: datetime | None) -> datetime:
         if value:
-            return value.replace(tzinfo=ZoneInfo(config.TIMEZONE))
+            _tz = config.TIMEZONE if config.TIMEZONE else "Asia/Manila"
+            local_tz = pytz.timezone(_tz)
+            local_dt = value.replace(tzinfo=pytz.utc).astimezone(local_tz)
+            return local_dt
 
 
 class Pagination(BaseModel):
